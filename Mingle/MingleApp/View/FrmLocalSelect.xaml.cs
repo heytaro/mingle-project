d1533379b4a4f3e97bdb859caedfb2a87a5b7d5c
Nathália Oliveira <nath.lo89@gmail.com>
2014-11-11 22:28:03 -0400

    Revert "Revert "#2""
    This reverts commit 5228f7c004bdbf1bc3287d2a04785de586c348ba.

--- Mingle/MingleApp/View/FrmLocalSelect.xaml.cs
+++ Mingle/MingleApp/View/FrmLocalSelect.xaml.cs
@@ -15,41 +15,114 @@ using System.Windows.Shapes;
 using System.Windows.Media;
 using Microsoft.Phone.Maps.Controls;
 using MingleApp.Model;
+using Microsoft.Phone.Maps.Toolkit;
+using Windows.Devices.Geolocation;
+using System.Text;
 
 namespace MingleApp.View
 {
     public partial class FrmLocalSelect : PhoneApplicationPage
     {
         MapManager mpMng = new MapManager();
+        Boolean hasNamedLocal = false, hasPushpined = false;
 
         public FrmLocalSelect()
         {
             InitializeComponent();
         }
 
-        private void Map_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            //  Point p = e.GetPosition(map);
-            //GeoCoordinate s = map.ConvertViewportPointToGeoCoordinate(p);
+ 	         base.OnNavigatedTo(e);
+             myLocation();
+        }
 
-            //   mpMng.Maps_ReverseGeoCoding(s);
-            //txtEndereco.Text = mpMng.sb.ToString();
+        async public void myLocation()
+        {
+            var geolocator = new Geolocator() { DesiredAccuracyInMeters = 10 };
 
-            Ellipse myCircle = new Ellipse();
-            myCircle.Fill = new SolidColorBrush(Colors.Blue);
-            myCircle.Height = 20;
-            myCircle.Width = 20;
-            myCircle.Opacity = 50;
+            var geoposition = await geolocator.GetGeopositionAsync();
 
-            MapOverlay myLocationOverlay = new MapOverlay();
-            myLocationOverlay.Content = myCircle;
-            myLocationOverlay.PositionOrigin = new Point(0, 0);
-            //myLocationOverlay.GeoCoordinate = s;
+            map.Center = geoposition.Coordinate.ToGeoCoordinate();
+            map.ZoomLevel = 13;
+            Pushpin userPin = new Pushpin()
+            {
+                Content = "Minha Localização",
+                GeoCoordinate = map.Center
+            };
+            MapOverlay overlay = new MapOverlay()
+            {
+                GeoCoordinate = map.Center,
+                Content = userPin
+            };
+            MapLayer layer = new MapLayer();
+            layer.Add(overlay);
 
-            MapLayer myLocationLayer = new MapLayer();
-            myLocationLayer.Add(myLocationOverlay);
+            map.Layers.Add(layer);
 
-            //map.Layers.Add(myLocationLayer);   
+            hasPushpined = true;
+            hasNamedLocal = false;
+            txtEndereco.Text = "Dê um nome para o local selecionado";
+            txtEndereco.Foreground = new SolidColorBrush(Colors.DarkGray);
+        }
+
+
+        async private void Map_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            try
+            {
+                //Get the current position
+                var geolocator = new Geolocator() { DesiredAccuracyInMeters = 10 };
+
+                Point p = e.GetPosition(map);
+                var geoposition = map.ConvertViewportPointToGeoCoordinate(p);
+
+                //Perform the reverse geocode query 
+                var query = new ReverseGeocodeQuery() { GeoCoordinate = geoposition }; 
+                var geoCodeResults = await query.GetMapLocationsAsync();
+                var address = geoCodeResults.First().Information.Address; 
+
+                this.newLayer(FormatAddress(address), geoposition);
+            }
+            catch (Exception ex)
+            {
+                if ((uint)ex.HResult == 0x80004004)
+                {
+                    //Location services turned off, ask user to turn it on.
+                    AskUserToTurnOnLocationServices();
+                }
+                else
+                {
+                    MessageBox.Show("Algo deu errado ao tentarmos acessar sua localização :((");
+                }
+            }
+        }
+
+        private static async void AskUserToTurnOnLocationServices()
+        {
+            var result = MessageBox.Show("Esse aplicativo demanda acesso à sua localização", "Gostaria de habilitar seus recursos de localização?", MessageBoxButton.OKCancel);
+
+            if (result == MessageBoxResult.OK)
+            {
+                await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-location:"));
+            }
+        }
+
+        private string FormatAddress(MapAddress address)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(address.Street).Append(",")
+                .Append(address.HouseNumber).Append("\n")
+                .Append(address.City).Append("\n").Append(address.StateCode).Append("\n")
+                .Append(address.Country).Append("\n").Append(address.Neighborhood);
+
+            return sb.ToString();
+        }
+
+        private string FormatCoordinates(Geoposition geoposition)
+        {
+            return String.Format("Latitude: {0}\nLongitude: {1}", geoposition.Coordinate.Latitude, geoposition.Coordinate.Longitude);
         }
 
         private void Avancar_Click(object sender, EventArgs e)
@@ -72,6 +145,54 @@ namespace MingleApp.View
         private void Voltar_Click(object sender, EventArgs e)
         {
             NavigationService.GoBack();
+        }
+
+        private void newLayer(string content, GeoCoordinate s)
+        {
+            map.Layers.Clear();
+
+            Point p = map.ConvertGeoCoordinateToViewportPoint(s);
+            Pushpin local = new Pushpin();
+            local.Content = content;
+            local.Height = 150;
+            p.Y -= local.Height;
+            s = map.ConvertViewportPointToGeoCoordinate(p);
+            local.GeoCoordinate = s;
+
+            MapLayer layer1 = new MapLayer();
+            MapOverlay overlay1 = new MapOverlay();
+            overlay1.Content = local;
+            overlay1.GeoCoordinate = s;
+            layer1.Add(overlay1);
+            s = map.ConvertViewportPointToGeoCoordinate(p);
+            local.GeoCoordinate = s;
+            map.Layers.Add(layer1);
+
+            hasPushpined = true;
+            hasNamedLocal = false;
+        }
+
+        private void txtEndereco_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtEndereco.Text == "")
+            {
+                txtEndereco.Foreground = new SolidColorBrush(Colors.Gray);
+                if (!hasPushpined) txtEndereco.Text = "Marque no mapa o local do encontro";
+                if (hasPushpined)
+                {
+                    txtEndereco.Text = "Dê um nome para o local selecionado";
+                    txtEndereco.Foreground = new SolidColorBrush(Colors.DarkGray);
+                }
+
+            }
+            else
+            {                    
+                if (!(txtEndereco.Text == "Marque no mapa o local do encontro") && !(txtEndereco.Text == "Dê um nome para o local selecionado"))
+                {
+                    hasNamedLocal = true;
+                    txtEndereco.Foreground = new SolidColorBrush(Colors.Purple);
+                }
+            }
         }
     }
 }
